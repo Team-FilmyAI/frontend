cee18698f4e69af562d7e9bbd1bc80c903e292cc
Christian Concepcion <70904084+cetycon26@users.noreply.github.com>
2025-06-16 15:40:40 -0400

    Created data/constants.js
    This file will contain global data for the team to use for the App. I am using this to store film data.

--- src/pages/FilmProjectDetails/FilmProjectDetails.jsx
+++ src/pages/FilmProjectDetails/FilmProjectDetails.jsx
@@ -1,62 +1,8 @@
 // import "./App.css";
 import "../../styles/FilmProjectDetails/FilmProjectDetails.css";
+import { movieDrive as movie } from "../../assets/data/constants.js";
 import { Link } from "react-router-dom";
 import { useState } from "react";
-
-const movie = {
-  title: "Drive",
-  posterUrl:
-    "https://s3.amazonaws.com/nightjarprod/content/uploads/sites/249/2023/12/19103252/602vevIURmpDfzbnv5Ubi6wIkQm.jpg",
-  genres: "Drama/Romance",
-  synopsis:
-    "This action drama follows a mysterious man who has multiple jobs as a garage mechanic, a Hollywood stuntman and a getaway driver seems to be trying to escape his shady past as he falls for his neighbor - whose husband is in prison and who's looking after her child alone. Meanwhile, his garage mechanic boss is trying to set up a race team using gangland money, which implicates our driver as he is to be used as the race team's main driver. Our hero gets more than he bargained for when he meets the man who is married to the woman he loves.",
-  director: "Nicolas Winding Refn",
-  filmingDates: {
-    start: "2010-09-25",
-    end: "2010-11-12",
-  },
-  budget: "$15 million",
-  productionTime: "7 weeks",
-  productionCompanies: [
-    "Bold Films",
-    "Marc Platt Productions",
-    "OddLot Entertainment",
-  ],
-  filmingLocations: [
-    "Los Angeles, California, USA",
-    "Downtown LA",
-    "Echo Park",
-    "Griffith Park",
-  ],
-  availableRoles: [
-    {
-      roleName: "Supporting Male Lead",
-      description:
-        "Charming and witty character who becomes Sarah's love interest.",
-      requirements: [
-        "Age: 25-35",
-        "emotional range",
-        "subtle expression",
-        "chemistry with male lead",
-      ],
-      payRange: [55000, 75000],
-      availability: true,
-    },
-    {
-      roleName: "Standard",
-      description:
-        "Irene’s husband, recently released from prison, trying to go straight.",
-      requirements: [
-        "Age: 30-40",
-        "intense screen presence",
-        "physical acting",
-        "stunt coordination experience",
-      ],
-      payRange: [35000, 45000],
-      availability: false,
-    },
-  ],
-};
 
 function App() {
   return (
