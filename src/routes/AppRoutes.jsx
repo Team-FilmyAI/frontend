7b19e17b25119e6424882455034f8a488270051b
Christian Concepcion <70904084+cetycon26@users.noreply.github.com>
2025-06-16 15:16:55 -0400

    Added: variables.css, FilmProjectDetails.jsx and .css, FilmDetails temporarily accessible thru AppRoutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,6 +4,7 @@ import Login from "../pages/LoginSignUp/Login";
 import SignUp from "../pages/LoginSignUp/SignUp";
 import Forgot from "../pages/LoginSignUp/Forgot";
 import ProfilePage from "../pages/ProfilePage/ProfilePage";
+import FilmProjectDetails from "../pages/FilmProjectDetails/FilmProjectDetails";
 export default function AppRoutes() {
   return (
     <Routes>
@@ -12,6 +13,8 @@ export default function AppRoutes() {
       <Route path="/SignUp" element={<SignUp />} />
       <Route path="/Forgot" element={<Forgot />} />
       <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/FilmDetails" element={<FilmProjectDetails />} />
+      {/* Will remove /FilmDetails from AppRoutes and connect it to Landing page once it's complete */}
     </Routes>
   );
 }
