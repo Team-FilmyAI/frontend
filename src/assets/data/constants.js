ab3d3144b8a787587262ae20b30c1a6771cf601a
Christian Concepcion <70904084+cetycon26@users.noreply.github.com>
2025-07-04 15:43:49 -0400

    Push Radio component + necessary misc. structuring modifications

--- src/assets/data/constants.js
+++ src/assets/data/constants.js
@@ -1,54 +1,71 @@
+import { formStyles } from '../../components/Dropdown/DropdownStyles';
+
+// FilmProjectDetails
 export const movieDrive = {
-  title: "Drive in Manhattan",
+  title: 'Drive in Manhattan',
   posterUrl:
-    "https://s3.amazonaws.com/nightjarprod/content/uploads/sites/249/2023/12/19103252/602vevIURmpDfzbnv5Ubi6wIkQm.jpg",
-  genres: "Drama/Romance",
+    'https://s3.amazonaws.com/nightjarprod/content/uploads/sites/249/2023/12/19103252/602vevIURmpDfzbnv5Ubi6wIkQm.jpg',
+  genres: 'Drama/Romance',
   synopsis:
     "This action drama follows a mysterious man who has multiple jobs as a garage mechanic, a Hollywood stuntman and a getaway driver seems to be trying to escape his shady past as he falls for his neighbor - whose husband is in prison and who's looking after her child alone. Meanwhile, his garage mechanic boss is trying to set up a race team using gangland money, which implicates our driver as he is to be used as the race team's main driver. Our hero gets more than he bargained for when he meets the man who is married to the woman he loves.",
-  director: "Nicolas Winding Refn",
+  director: 'Nicolas Winding Refn',
   filmingDates: {
-    start: "2010-09-25",
-    end: "2010-11-12",
+    start: '2010-09-25',
+    end: '2010-11-12',
   },
-  budget: "$15 million",
-  productionTime: "7 weeks",
-  productionCompanies: [
-    "Bold Films",
-    "Marc Platt Productions",
-    "OddLot Entertainment",
-  ],
-  filmingLocations: [
-    "Los Angeles, California, USA",
-    "Downtown LA",
-    "Echo Park",
-    "Griffith Park",
-  ],
+  budget: '$15 million',
+  productionTime: '7 weeks',
+  productionCompanies: ['Bold Films', 'Marc Platt Productions', 'OddLot Entertainment'],
+  filmingLocations: ['Los Angeles, California, USA', 'Downtown LA', 'Echo Park', 'Griffith Park'],
   availableRoles: [
     {
-      roleName: "Supporting Male Lead",
-      description:
-        "Charming and witty character who becomes Sarah's love interest.",
+      roleName: 'Supporting Male Lead',
+      description: "Charming and witty character who becomes Sarah's love interest.",
       requirements: [
-        "Age: 25-35",
-        "emotional range",
-        "subtle expression",
-        "chemistry with male lead",
+        'Age: 25-35',
+        'emotional range',
+        'subtle expression',
+        'chemistry with male lead',
       ],
       payRange: [55000, 75000],
       availability: true,
     },
     {
-      roleName: "Standard",
-      description:
-        "Irene’s husband, recently released from prison, trying to go straight.",
+      roleName: 'Standard',
+      description: 'Irene’s husband, recently released from prison, trying to go straight.',
       requirements: [
-        "Age: 30-40",
-        "intense screen presence",
-        "physical acting",
-        "stunt coordination experience",
+        'Age: 30-40',
+        'intense screen presence',
+        'physical acting',
+        'stunt coordination experience',
       ],
       payRange: [35000, 45000],
       availability: false,
     },
   ],
+};
+
+// Dropdown
+export const testDropdown = {
+  placeholder: 'Pick your Hero...',
+  isMulti: true,
+  styles: formStyles,
+  options: [
+    { value: 'spiderMan', label: 'Spider Man' },
+    { value: 'ironMan', label: 'Iron Man' },
+    { value: 'venom', label: 'Venom' },
+  ],
+};
+
+// Radio
+export const progLangs = {
+  radioName: 'languages',
+  options: ['HTML', 'CSS', 'Python', 'Java', 'C++'],
+  newLine: false,
+};
+
+export const yesOrNo = {
+  radioName: 'yesNo',
+  options: ['Yes', 'No'],
+  newLine: false,
 };
