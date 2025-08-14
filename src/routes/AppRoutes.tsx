d3b9dd698e0e77eecf64e7bc8738e3c9297cd928
Santhosh Charudatta <santhoshcharudatta@Santhoshs-MacBook-Air.local>
2025-08-14 00:50:37 -0400

    Resolve AppRoutes.tsx conflict, add ApplicationReview route, update packages, added applicationReview files and shared ui components

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -6,11 +6,12 @@ import Login from '../pages/Login/Login';
 import ProductionDashboard from '../pages/ProductionDashboard/ProductionDashboard';
 import ProfilePage from '../pages/ProfilePage/ProfilePage';
 import SignUp from '../pages/SignUp/SignUp';
-import FilmProjectDetails from './../pages/FilmProjectDetails/FilmProjectDetails';
+// import FilmProjectDetails from '../pages/FilmProjectDetails/FilmProjectDetails';
 import LandingPage from '../pages/LandingPage/LandingPage';
 import ViewOpportunities from '../pages/ViewOpportunities/ViewOpportunities';
 import RoleDetailPage from '../pages/RoleDetailPage/RoleDetailPage';
 
+import ApplicationReview from '../pages/ApplicationReview/ApplicationReview';
 
 export default function AppRoutes() {
   return (
@@ -20,12 +21,13 @@ export default function AppRoutes() {
       <Route path="/SignUp" element={<SignUp />} />
       <Route path="/Forgot" element={<ForgotPassword />} />
       <Route path="/profile" element={<ProfilePage />} />
-      <Route path="/FilmDetails" element={<FilmProjectDetails />} />
+      {/* <Route path="/FilmDetails" element={<FilmProjectDetails />} /> */}
       <Route path="/ProductionDashboard" element={<ProductionDashboard />} />
       <Route path="/CurrentProjectDetails" element={<CurrentProjectDetails />} />
       <Route path="/LandingPage" element={<LandingPage />} />
       <Route path="/ViewOpportunities" element={<ViewOpportunities />} />
       <Route path="/RoleDetailPage" element={<RoleDetailPage />} />
+      <Route path="/ApplicationReview" element={<ApplicationReview />} />
     </Routes>
   );
 }
